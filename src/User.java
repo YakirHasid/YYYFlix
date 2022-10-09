671b829067e3a4ec48b25d117c3a0d16102ae607
YakirHasid <hasidyakir@gmail.com>
2022-10-09 14:34:01 +0300

    Moved isPaymentMethodValid to User Class comments

--- src/User.java
+++ src/User.java
@@ -9,6 +9,7 @@ public class User implements Serializable {
     private String paymentMethod;
 
     private static final int MIN_PASSWORD_LEN = 6;
+    private static final String[] VALID_PAYMENT_METHODS = {"PayPal", "VISA"};
 
     // public constructor
     public User(String username, String password, String name, String paymentMethod)
@@ -54,16 +55,45 @@ public class User implements Serializable {
         return password.length()>=MIN_PASSWORD_LEN;
     }
 
+    /**
+     * checks if the given payment method is a valid form of payment from the VALID_PAYMENT_METHODS array
+     * @param payment represents the given payment method
+     * @return a valid matching payment method (case corrected if necessary), null if given payment is not valid
+     */
+    public static String isPaymentMethodValid(String payment)
+    {
+        for (String paymentMethod :
+                VALID_PAYMENT_METHODS) {
+            if(payment.equalsIgnoreCase(paymentMethod))
+                return paymentMethod;
+        }
+        return null;
+    }
+
+    /**
+     * masks the current user's password with '*'
+     * @return a masked password, so that each character is replaced by '*'
+     *         password of "123456" => "******"
+     *         password of "abc" => "***"
+     */
     private String maskPassword()
     {
         return "*".repeat(this.password.length());
     }
 
+    // username getter
     public String getUsername()
     {
         return this.username;
     }
 
+    /**
+     * password setter with validation
+     * (external database update is assumed to be handled by the caller)
+     * @param password represents the updated password
+     * @return true if the given password is valid and local password update has been successfully completed
+     *         false if the given password is not valid
+     */
     public boolean setPassword(String password) {
         // check if password is valid
         if(!isPasswordValid(password))
@@ -75,9 +105,12 @@ public class User implements Serializable {
         return true;
     }
 
+    // name getter
     public String getName() {
         return this.name;
     }
+
+    // name setter (external database update is assumed to be handled by the caller)
     public boolean setName(String username) {
         // TODO: Implement
         return false;
@@ -87,8 +120,25 @@ public class User implements Serializable {
     {
         return this.paymentMethod;
     }
-    public boolean setPaymentMethod(String username) {
-        // TODO: Implement
+
+    /**
+     * payment method setter with validation
+     * (external database update is assumed to be handled by the caller)
+     * @param paymentMethod represents the updated payment method
+     * @return true if the given password is valid and local password update has been successfully completed
+     *         false if the given password is not valid
+     */
+    public boolean setPaymentMethod(String paymentMethod) {
+        // check for valid payment method checker
+        String payment = isPaymentMethodValid(paymentMethod);
+
+        // if invalid return false
+        if(payment == null)
+            return false;
+
+        // payment method is valid, update payment method and return true
+        // (external database update is assumed to be handled by the caller)
+        this.paymentMethod = payment;
         return false;
     }
 
